71c89df0faf6f4c6b71828d4b93463d7af4fddc3
Splendid <ndubuisixtiangod@gmail.com>
2023-08-21 15:51:25 +0100

    another commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let input = document.getElementById("input");
 let main = document.getElementById("main");
+let facebookIcon = document.getElementById("facebook-icon");
+let twitterIcon = document.getElementById("twitter-icon");
+let instagramIcon = document.getElementById("instagram-icon");
 
 function searchBook() {
   event.preventDefault();
@@ -7,7 +10,7 @@ function searchBook() {
   fetch(api)
     .then((response) => response.json())
     .then((data) => {
-        main.innerText = ""
+      main.innerText = "";
       let item = data.items;
       item.map((book) => {
         let books = document.createElement("div");
@@ -27,4 +30,63 @@ function searchBook() {
     .catch((err) => alert("error"));
 
   input.value = "";
+}
+
+function redirectToFacebook() {
+  
+  
+  let facebookLink = document.createElement("a");
+ 
+  facebookLink.href = "https://www.facebook.com/profile.php?id=100081341208797";
+ 
+  facebookLink.target = "_blank";
+  
+  facebookLink.innerHTML =
+    '<i  class="bi bi-facebook" style="color: blue"></i>';
+ 
+
+  facebookIcon.parentNode.replaceChild(facebookLink, facebookIcon);
+ 
+  window.location.href =
+    "https://www.facebook.com/profile.php?id=100081341208797";
+ 
+}
+
+
+function Twitter() {
+ 
+  
+ 
+  let twitterLink = document.createElement("a");
+ 
+  twitterLink.href = "https://twitter.com/centsplendor3?t=UWqbHihMR0bHyiX80xNkDg&s=09";
+  
+  twitterLink.target = "_blank";
+ 
+  twitterLink.innerHTML =
+    '<i class="bi bi-twitter" style="color: blue"></i>';
+
+  
+  twitterIcon.parentNode.replaceChild(twitterLink, twitterIcon);
+
+  
+  window.location.href =
+    "https://twitter.com/centsplendor3?t=UWqbHihMR0bHyiX80xNkDg&s=09";
+}
+
+
+function Instagram() {
+
+let instagramLink = document.createElement("a");
+instagramLink.href =
+  "https://instagram.com/splendor_chris3?igshid=NTc4MTIwNjQ2YQ==";
+
+instagramLink.target = "_blank";
+
+ instagramLink.innerHTML = '<i class="bi bi-instagram" style="color: purple"></i>';
+
+instagramIcon.parentNode.replaceChild(instagramLink, instagramIcon);
+
+ window.location.href =
+   "https://instagram.com/splendor_chris3?igshid=NTc4MTIwNjQ2YQ==";
 }
